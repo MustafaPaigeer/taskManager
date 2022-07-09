cf36c71566875ee785a13f204e5e32324c716445
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-08 23:51:23 -0700

    db connection  minor correction

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-require('./db/connect')
-const express = require('express');
-const app = express();
-const tasks = require('./routes/tasks');
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
-app.use(express.json());
+app.use(express.json())
 
 //routes
 app.get('/hello', (req, res) => {
@@ -21,6 +22,15 @@ app.use('/api/v1/tasks', tasks)
 
 
 const port = 3000;
-app.listen(port, console.log(`Server is listening on port ${port}...`));
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
 
